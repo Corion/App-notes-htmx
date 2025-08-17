4b3ea640d71881706877bfb10a1020f75ff5e1fa
Max Maischein <corion@corion.net>
2025-08-17 12:27:06 +0200

    Regenerate README from within Makefile.PL

--- lib/App/Notetaker.pm
+++ lib/App/Notetaker.pm
@@ -5,12 +5,37 @@ use 5.020;
 
 App::Notetaker - a markdown notetaker
 
+Closely positioned as another Google Keep clone
+
 =cut
 
 =head1 LIVE DEMO
 
 The live demo lives at
 L<https://datenzoo.de/notes/> ( C<< demo:demo >> )
+
+# LIVE DEMO
+
+The live demo lives at
+[https://datenzoo.de/notes/](https://datenzoo.de/notes/) ( `demo:demo` )
+
+# Screenshots
+
+## Firefox Desktop with [masonry layout](https://developer.mozilla.org/de/docs/Web/CSS/CSS_grid_layout/Masonry_layout)
+
+![Firefox with the experimental masonry layout](screenshots/firefox-masonry.png)
+
+## Firefox mobile
+
+![Firefox mobile](screenshots/firefox-mobile-nomasonry.png)
+
+## Chromium
+
+![Chromium](screenshots/chrome-new-from-template.png)
+
+## Dark Mode
+
+![Dark mode](screenshots/dark-mode.png)
 
 =cut
 
